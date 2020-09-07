505fec580eb3c96fafbcf49d171ad706b40affb6
Bayron <bayron_fabian@hotmail.com>
2020-09-07 19:12:11 -0300

    Crea función App

--- App.js
+++ App.js
@@ -3,5 +3,11 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 
 function App(){
-    
+    return (
+        <div className="App">
+            <h1>Hello Bayron</h1>
+            <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h2>
+            <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
+        </div>
+    );
 }
